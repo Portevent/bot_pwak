6a0c097a7856ff31250a0a4f398d82869600240c
BMS <local@provexi.fr>
2020-12-01 11:37:03 +0100

    Debug

---
+++ bots/nowel/commands/log.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+module.exports = {
+    name: 'log',
+    description: {
+        "fr": "Affiche les logs",
+        "en": "Show log"
+    },
+    admin: true,
+    execute(message, args) {
+        let log = require("./log.log");
+        message.author.send(log);
+    },
+};
