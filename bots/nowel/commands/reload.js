85edb82a53d4066cf1672295983e7eaeef70163e
BMS <local@provexi.fr>
2020-12-01 11:34:59 +0100

    Debug

--- bots/nowel/commands/reload.js
+++ bots/nowel/commands/reload.js
@@ -12,8 +12,9 @@ module.exports = {
         const command = message.client.commands.get(commandName)
             || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-        delete require.cache[require.resolve(`./${command.name}.js`)];
+        if (command) {
+            delete require.cache[require.resolve(`./${command.name}.js`)];
+        }
         try {
             const newCommand = require(`./${command.name}.js`);
             message.client.commands.set(newCommand.name, newCommand);
