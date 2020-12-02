d3ab90bbc9334515cc6dc50b20f37dfafab0bc69
BMS <local@provexi.fr>
2020-12-02 13:36:47 +0100

    Quick fix

---
+++ bots/nowel/commands/resetongoingloot.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'resetongoingloot',
+    description: {
+        "fr": "Reset un loot en cours (debug purpose)",
+        "en": "Reset ongoing loot (debug purpose)"
+    },
+    admin: true,
+    args: 1,
+    async execute(message, args) {
+        message.client.onGoingLoot.delete(args[0]);
+    },
+};
