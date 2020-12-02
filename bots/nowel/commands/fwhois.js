6babea7b0fc6f3bda7d547cd341315c5f01e4475
BMS <local@provexi.fr>
2020-12-02 10:19:35 +0100

    Quick fix

---
+++ bots/nowel/commands/fwhois.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'fwhois',
+    description: {
+        "fr": "Retourne un utilisateur (force cache skip)",
+        "en": "Show an user (force cache skip)"
+    },
+    admin: true,
+    args: 1,
+    async execute(message, args) {
+        message.client.users.fetch(args[0], true, true).then(user => {
+            message.reply('', {
+                'embed': {
+                    "description": "Id : " + user.id,
+                    "author": {
+                        "name": user.username,
+                        "icon_url": user.avatarURL()
+                    }
+                }
+            })
+        }).catch(
+            err => message.client.logErrorMsg(err, message)
+        )
+    },
+};
