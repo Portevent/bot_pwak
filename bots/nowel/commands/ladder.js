ee8f46e7e6041761119aa858e5630af12fd96ac6
Portevent <Portevent@protonmail.com>
2020-12-07 21:29:45 +0100

    Allow ladder in offTopic

--- bots/nowel/commands/ladder.js
+++ bots/nowel/commands/ladder.js
@@ -9,6 +9,7 @@ module.exports = {
         "en": "Show the leaderboard"
     },
     admin: true,
+    delete: true,
     async execute(message, args) {
         const language = message.client.getLanguage(message.channel);
         let leaderboard = [];
@@ -75,6 +76,7 @@ module.exports = {
             message.author.send(ladder).catch(e => message.client.logErrorMsg(e, message));
         }
         else{
+            console.log(message.client.offTopics[language]);
             message.client.sendWebhook(message.client.offTopics[language], {
                 "content": ">PIKPIK_DELETE_ME",
                 "username": {fr:"Classement",en:"Ladder"}[language],
