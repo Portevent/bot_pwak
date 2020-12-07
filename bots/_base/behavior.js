62cfd3031d2640fb84404ff41d16fa433dc0fcf9
Portevent <Portevent@protonmail.com>
2020-12-07 21:42:26 +0100

    Add better log err

--- bots/_base/behavior.js
+++ bots/_base/behavior.js
@@ -73,7 +73,7 @@ module.exports = {
     onMessage(message) {
         const client = message.client;
         if(message.content.startsWith('> ')) return;
-        if(message.content.startsWith('>PIKPIK_DELETE_ME')) return message.delete();
+        if(message.content.startsWith('>PIKPIK_DELETE_ME')) return message.delete({timeout: 500});
         if(message.author.id === client.user.id) return client.onOwnMessage(message);
         
         // Doesn't require to specify the prefix for dm
