4129468c575a70c652e1d590309f3705fa59bc77
BMS <local@provexi.fr>
2020-12-07 14:05:44 +0100

    Add inventory spy trought InvSpy item

--- bots/nowel/commands/inventory.js
+++ bots/nowel/commands/inventory.js
@@ -7,7 +7,24 @@ module.exports = {
         "en": "Display the inventory"
     },
     delete: true,
-    execute(message, args) {
-        message.client.sendInventory(message.author, message.author);
+    async execute(message, args) {
+        let target = message.author;
+        if(args.length && message.client.inventory.userHasItem(message.author.id, 'invspy')){
+            const name = args[0].toLowerCase();
+            for(let userId of message.client.inventory.inventory.keys()){
+                let member = (message.guild?await message.guild.members.fetch(userId):await message.client.referenceGuild.members.fetch(userId));
+                if (member
+                    && (
+                        (member.nickname && member.nickname.toLowerCase().match(new RegExp(name)))
+                        ||
+                        member.user.username.toLowerCase().match(new RegExp(name))
+                        )
+                    ){
+                    target = member.user;
+                    break;
+                }
+            }
+        }
+        message.client.sendInventory(target, message.author);
     },
 };
